fd94d853bc16e0c5f3b717e09f29c58bb3a77df5
John-Kim Murphy <381217+Leftium@users.noreply.github.com>
2024-12-23 08:12:08 +0900

    Add fullscreen button and experimental AB repeat.

--- src/lib/player/Player.svelte
+++ src/lib/player/Player.svelte
@@ -1,21 +1,62 @@
 <script lang="ts">
 	import type { MediaPlayerElement } from 'vidstack/elements';
 	import 'vidstack/bundle';
+	import 'vidstack/icons';
+	import { onMount } from 'svelte';
 
 	let player: MediaPlayerElement;
 
-	function onclick() {
-		player.paused = !player.paused;
+	const framerate = 30;
+	const repeatA = 25;
+	const repeatB = 40;
+
+	onMount(() => {
+		if (!player) {
+			return;
+		}
+
+		const unsubscribe = player.subscribe((e) => {
+			if (e.currentTime < repeatA || e.currentTime > repeatB) {
+				player.currentTime = repeatA;
+			}
+		});
+
+		return function () {
+			unsubscribe();
+		};
+	});
+
+	function onclick(e: MouseEvent) {
+		if (e.shiftKey) {
+			player.currentTime += 1 / framerate;
+		} else {
+			player.paused = !player.paused;
+		}
 	}
 </script>
 
-<media-player
-	bind:this={player}
-	{onclick}
-	playsInline
-	title="Sprite Fight"
-	src="youtube/_cMxraX_5RE"
-	role="none"
->
-	<media-provider></media-provider>
+<media-player bind:this={player} playsInline src="youtube/dt-SqNL4z3w">
+	<media-provider {onclick} role="none"></media-provider>
+
+	<media-controls class="vds-controls">
+		<div class="vds-controls-spacer"></div>
+		<media-controls-group class="vds-controls-group">
+			<media-fullscreen-button class="vds-button">
+				<media-icon type="fullscreen" class="fs-enter-icon vds-icon"></media-icon>
+				<media-icon type="fullscreen-exit" class="fs-exit-icon vds-icon"></media-icon>
+			</media-fullscreen-button>
+		</media-controls-group>
+	</media-controls>
 </media-player>
+
+<style>
+	.vds-controls-group {
+		display: flex;
+		flex-direction: row-reverse;
+	}
+
+	:global(.vds-button[data-active] .fs-enter-icon),
+	.vds-button:not([data-active]) .fs-exit-icon {
+		display: none;
+	}
+</style>
