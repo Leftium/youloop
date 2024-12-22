b5992c0804b8e9c086c1bd0e94ce0c94b81fce09
John-Kim Murphy <381217+Leftium@users.noreply.github.com>
2024-12-23 05:00:39 +0900

    Install vidstack.
    https://vidstack.io/docs/player/getting-started/installation/svelte/?bundler=sveltekit&provider=youtube&styling=css

---
+++ src/lib/player/Player.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import type { MediaPlayerElement } from 'vidstack/elements';
+	import 'vidstack/bundle';
+
+	let player: MediaPlayerElement;
+
+	function onclick() {
+		player.paused = !player.paused;
+	}
+</script>
+
+<media-player
+	bind:this={player}
+	{onclick}
+	playsInline
+	title="Sprite Fight"
+	src="youtube/_cMxraX_5RE"
+	role="none"
+>
+	<media-provider></media-provider>
+</media-player>
