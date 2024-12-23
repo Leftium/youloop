76851028b7e161a18fec46853c3743c9e6260de6
John-Kim Murphy <381217+Leftium@users.noreply.github.com>
2024-12-23 12:57:49 +0900

    Remove SvelteKit template content.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,4 @@
 	import Player from '$lib/player/Player.svelte';
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-
 <Player></Player>
